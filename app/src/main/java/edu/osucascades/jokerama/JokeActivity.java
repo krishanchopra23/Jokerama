5b4ed67a68270cbf07bc4a9222fdfa79d8109513
Krishan <krishanchopra30@yahoo.com>
2018-11-17 11:55:17 -0800

    Create an abstract fragment class

--- app/src/main/java/edu/osucascades/jokerama/JokeActivity.java
+++ app/src/main/java/edu/osucascades/jokerama/JokeActivity.java
@@ -1,13 +1,14 @@
 package edu.osucascades.jokerama;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
-public class JokeActivity extends AppCompatActivity {
+public class JokeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_joke);
+    protected Fragment createFragment() {
+        return new JokeFragment();
     }
 }
