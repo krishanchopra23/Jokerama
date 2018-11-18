562dcf759e2952ddc2bc9b09bf665002dd1e31fe
Krishan <krishanchopra30@yahoo.com>
2018-11-17 19:34:11 -0800

    Create an adapter

--- app/src/main/java/edu/osucascades/jokerama/JokeListFragment.java
+++ app/src/main/java/edu/osucascades/jokerama/JokeListFragment.java
@@ -1,12 +1,15 @@
 package edu.osucascades.jokerama;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 public class JokeListFragment extends Fragment {
 
@@ -24,6 +27,31 @@ public class JokeListFragment extends Fragment {
     private class JokeHolder extends RecyclerView.ViewHolder {
         public JokeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_joke, parent, false));
+        }
+    }
+
+    private class JokeAdapter extends RecyclerView.Adapter<JokeHolder> {
+
+        private List <Joke> mJokes;
+
+        public JokeAdapter(List <Joke> jokes) {
+            mJokes = jokes;
+        }
+
+        @Override
+        public JokeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new JokeHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(JokeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mJokes.size();
         }
     }
 }
