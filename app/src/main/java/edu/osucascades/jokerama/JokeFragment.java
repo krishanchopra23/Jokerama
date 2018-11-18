57b69bae33ac6f5aa2cce3ef7a65d4afbf5bb4c1
Krishan <krishanchopra30@yahoo.com>
2018-11-18 10:38:15 -0800

    Retreive the extra id

--- app/src/main/java/edu/osucascades/jokerama/JokeFragment.java
+++ app/src/main/java/edu/osucascades/jokerama/JokeFragment.java
@@ -7,22 +7,46 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.UUID;
+
 public class JokeFragment extends Fragment {
 
     private Joke mJoke;
     private TextView mJokeTextView;
+    private int mLineJoke;
     //overriding on CreateView
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mJoke = new Joke();
+        //retrieve the extra and fetch the joke
+        UUID jokeId = (UUID) getActivity().getIntent()
+                .getSerializableExtra(JokeActivity.EXTRA_JOKE_ID);
+        mJoke = JokeLab.get(getActivity()).getJoke(jokeId);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_joke, container, false);
         mJokeTextView = (TextView) v.findViewById(R.id.joke_fragment_text_view);
-        mJokeTextView.setText(mJoke.getLinesOfJoke()[0]);
+
+        mLineJoke = 0;
+        updateJokeLine(mLineJoke);
+        //set a listener on the textview
+        mJokeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLineJoke++;
+                updateJokeLine(mLineJoke);
+            }
+        });
         return v;
+    }
+    //update the joke when its done
+    public void updateJokeLine(int index) {
+        if (index >= 5) {
+            mJoke.setViewed(true);
+            return;
+        }
+        mJokeTextView.setText(mJoke.getLinesOfJoke()[index]);
     }
 }
