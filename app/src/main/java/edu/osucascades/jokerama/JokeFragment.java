6b673014a2ddb0b89df14a99162f5b9ace662f0c
Krishan <krishanchopra30@yahoo.com>
2018-11-19 11:53:48 -0800

    Attempted work in class

--- app/src/main/java/edu/osucascades/jokerama/JokeFragment.java
+++ app/src/main/java/edu/osucascades/jokerama/JokeFragment.java
@@ -13,6 +13,10 @@ public class JokeFragment extends Fragment {
 
     private Joke mJoke;
     private TextView mJokeTextView;
+    private TextView mJokeTextView2;
+    private TextView mJokeTextView3;
+    private TextView mJokeTextView4;
+    private TextView mJokeTextView5;
     private int mLineJoke;
     //overriding on CreateView
     @Override
@@ -28,14 +32,61 @@ public class JokeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_joke, container, false);
         mJokeTextView = (TextView) v.findViewById(R.id.joke_fragment_text_view);
+        mJokeTextView2 = (TextView) v.findViewById(R.id.joke_fragment_text_view2);
+        mJokeTextView3 = (TextView) v.findViewById(R.id.joke_fragment_text_view3);
+        mJokeTextView4 = (TextView) v.findViewById(R.id.joke_fragment_text_view4);
+        mJokeTextView5 = (TextView) v.findViewById(R.id.joke_fragment_text_view5);
+
+        mJokeTextView.setVisibility(View.VISIBLE);
+        mJokeTextView2.setVisibility(View.INVISIBLE);
+        mJokeTextView3.setVisibility(View.INVISIBLE);
+        mJokeTextView4.setVisibility(View.INVISIBLE);
+        mJokeTextView5.setVisibility(View.INVISIBLE);
 
         mLineJoke = 0;
         updateJokeLine(mLineJoke);
+
         //set a listener on the textview
         mJokeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mLineJoke++;
+                updateJokeLine(mLineJoke);
+            }
+        });
+
+        mJokeTextView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLineJoke++;
+                mJokeTextView2.setVisibility(View.VISIBLE);
+                updateJokeLine(mLineJoke);
+            }
+        });
+
+        mJokeTextView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLineJoke++;
+                mJokeTextView3.setVisibility(View.VISIBLE);
+                updateJokeLine(mLineJoke);
+            }
+        });
+
+        mJokeTextView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLineJoke++;
+                mJokeTextView4.setVisibility(View.VISIBLE);
+                updateJokeLine(mLineJoke);
+            }
+        });
+
+        mJokeTextView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLineJoke++;
+                mJokeTextView5.setVisibility(View.VISIBLE);
                 updateJokeLine(mLineJoke);
             }
         });
@@ -49,4 +100,8 @@ public class JokeFragment extends Fragment {
         }
         mJokeTextView.setText(mJoke.getLinesOfJoke()[index]);
     }
+
+    //public void updateJokeLine(TextView view, int index) {
+    //    view.setText(mJoke.getLinesOfJoke()[index]);
+    //}
 }
